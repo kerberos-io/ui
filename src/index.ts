c9762d6a977e676c7c3813d16e304e1efff5fa1c
cedricve <cedric@verstraeten.io>
2021-06-16 21:18:14 +0200

    upfeSW

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import Icon from "./components/Icon";
 import Button from "./components/Button";
 import Sidebar from "./components/Sidebar";
 import Profilebar from "./components/Profilebar";
 import Navigation from "./components/Navigation";
+import Icon from "./components/Icon";
 
-export { Button, Sidebar, Profilebar, Navigation };
+export { Button, Sidebar, Profilebar, Navigation, Icon };
