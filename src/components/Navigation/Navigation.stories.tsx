5af89223737b652e878d683dbd139cf0697613b4
cedricve <cedric@verstraeten.io>
2021-06-21 19:47:01 +0200

    fix navigation story

--- src/components/Navigation/Navigation.stories.tsx
+++ src/components/Navigation/Navigation.stories.tsx
@@ -17,37 +17,28 @@ export default {
     }
 } as Meta;
 
-// Create a master template for mapping args to render the Button component
-const Template: Story<NavigationItemProps> = (args) => <NavigationGroup>
-    <NavigationItem {...args} />
-</NavigationGroup>;
-
-// Reuse that template for creating different stories
-export const NavigationItemActive = Template.bind({});
-NavigationItemActive.args = { icon: "dashboard", title: "dashboard", active: true };
-
-export const NavigationWithItems = () => <Navigation>
+export const NavigationWithItems = () =><Navigation>
     <NavigationSection title={"monitoring"}/>
     <NavigationGroup>
-        <NavigationItem title={"Dashboard"} icon={"dashboard"} active={true}/>
-        <NavigationItem title={"Latest events"} icon={"activity"}/>
-        <NavigationItem title={"Livestreams"} icon={"livestream"}/>
-        <NavigationItem title={"Media"} icon={"media"}/>
+        <NavigationItem link={"dashboard"} title={"Dashboard"} icon={"dashboard"} active={true}/>
+        <NavigationItem link={"events"} title={"Latest events"} icon={"activity"}/>
+        <NavigationItem link={"livestream"} title={"Livestreams"} icon={"livestream"}/>
+        <NavigationItem link={"media"} title={"Media"} icon={"media"}/>
     </NavigationGroup>
     <NavigationSection title={"analysis"}/>
     <NavigationGroup>
-        <NavigationItem title={"Tasks"} icon={"tasks"}/>
+        <NavigationItem link={"tasks"} title={"Tasks"} icon={"tasks"}/>
     </NavigationGroup>
     <NavigationSection title={"integrations"}/>
     <NavigationGroup>
-        <NavigationItem title={"Alerts"} icon={"alerts"}/>
-        <NavigationItem title={"Channels"} icon={"channels"}/>
+        <NavigationItem link={"alerts"} title={"Alerts"} icon={"alerts"}/>
+        <NavigationItem link={"channels"} title={"Channels"} icon={"channels"}/>
     </NavigationGroup>
     <NavigationSection title={"management"}/>
     <NavigationGroup>
-        <NavigationItem title={"Cameras"} icon={"cameras"}/>
-        <NavigationItem title={"Sites"} icon={"sites"}/>
-        <NavigationItem title={"Accounts"} icon={"accounts"}/>
-        <NavigationItem title={"Plans & integrations"} icon={"subscription"}/>
+        <NavigationItem link={"cameras"} title={"Cameras"} icon={"cameras"}/>
+        <NavigationItem link={"sites"} title={"Sites"} icon={"sites"}/>
+        <NavigationItem link={"accounts"} title={"Accounts"} icon={"accounts"}/>
+        <NavigationItem link={"subscription"} title={"Plans & integrations"} icon={"subscription"}/>
     </NavigationGroup>
 </Navigation>
