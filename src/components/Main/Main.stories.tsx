1d802f0507fda949fb98b8dc51c21df55a0882fd
cedricve <cedric@verstraeten.io>
2021-07-15 22:59:35 +0200

    Getting rid of subdir

--- src/components/Main/Main.stories.tsx
+++ src/components/Main/Main.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Meta } from "@storybook/react/types-6-0";
 import { Story } from "@storybook/react";
-import { MainPage } from './stories/MainPage';
-import { LoginPage } from './stories/LoginPage';
+import { MainPage } from './MainPage';
+import { LoginPage } from './LoginPage';
 
 export default {
     title: "Components/Main",
