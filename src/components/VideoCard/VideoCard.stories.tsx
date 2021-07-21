4ee40d590ed4b377e737e5033288e4626f725da8
Recep Öztürk <recep.fed@gmail.com>
2021-07-21 17:20:40 +0300

    VideoCard import as named module

--- src/components/VideoCard/VideoCard.stories.tsx
+++ src/components/VideoCard/VideoCard.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Meta } from "@storybook/react/types-6-0"
 import {Story} from "@storybook/react"
-import VideoCard,{VideoCardProps} from "./VideoCard";
+import {VideoCard,VideoCardProps} from "./VideoCard";
 
 export default {
     title:"Components/VideoCard",
