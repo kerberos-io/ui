62703a16c9ace0793b3741bbbeab032a2e2b690a
Recep Öztürk <recep.fed@gmail.com>
2021-08-24 09:27:54 +0300

    Footer responsive & Controlbar refactore

--- src/components/ControlBar/Controlbar.stories.tsx
+++ src/components/ControlBar/Controlbar.stories.tsx
@@ -5,6 +5,7 @@ import { Input } from "../Input";
 import Button from "../Button";
 import { FilterSort,FilterSearch,FilterDisplay,FilterSite } from ".";
 import {ControlBar} from "./ControlBar"
+import { Dropdown } from "../Dropdown";
 
 export default {
     title:"Components/ControlBar",
@@ -36,4 +37,29 @@ export default {
         iconright="arrow-down-sm"
         />
     </FilterSort>
+</ControlBar>
+
+export const _ControlBar2 = () => <ControlBar>
+      <Input iconleft="search" placeholder="Search..." type="text" />
+      <Dropdown onChange direction="left" icon={"location"} placeholder={"Site"} items={[
+        {label: "Hannver, Germany", value: "Hannver, Germany"},
+        {label: "Düsseldorf, Germany", value: "Düsseldorf, Germany"},
+        {label: "Düsseldorf, Germany", value: "Düsseldorf, Germany"}
+    ]}/>
+    <Dropdown onChange direction="left" icon={"cameras"} placeholder={"Camera"} items={[
+        {label: "Hannver, Germany", value: "Hannver, Germany"},
+        {label: "Düsseldorf, Germany", value: "Düsseldorf, Germany"},
+        {label: "Düsseldorf, Germany", value: "Düsseldorf, Germany"}
+    ]}/>
+     <Dropdown onChange direction="left" icon={"preferences"} placeholder={"Classification"} items={[
+        {label: "Hannver, Germany", value: "Hannver, Germany"},
+        {label: "Düsseldorf, Germany", value: "Düsseldorf, Germany"},
+        {label: "Düsseldorf, Germany", value: "Düsseldorf, Germany"}
+    ]}/>
+     
+     <Dropdown onChange direction="left" icon={"sort"} placeholder={"Sort"} items={[
+        {label: "Hannver, Germany", value: "Hannver, Germany"},
+        {label: "Düsseldorf, Germany", value: "Düsseldorf, Germany"},
+        {label: "Düsseldorf, Germany", value: "Düsseldorf, Germany"}
+    ]}/>
 </ControlBar>
