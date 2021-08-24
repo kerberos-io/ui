45ef89a1894b1f82e6c8a4bfaff49a5c6dd7c34c
cedricve <cedric@verstraeten.io>
2021-08-24 13:20:33 +0200

    update border-radius: setup-box

--- src/components/SetupBox/SetupBox.tsx
+++ src/components/SetupBox/SetupBox.tsx
@@ -3,30 +3,32 @@ import "./setupbox.scss"
 import Badge from "../Badge";
 import Button from "../Button";
 
-export interface SetupBoxProps{
-  dashed?:boolean,
-  header:string,
-  text:string,
-  btnicon:string,
-  btnlabel:string
+export interface SetupBoxProps {
+    dashed?: boolean,
+    header: string,
+    text: string,
+    btnicon: string,
+    btnlabel: string
+    button: any
 }
 
-
-const SetupBox = ({  
-    dashed=false,
-    header,
-    text,
-    btnicon,
-    btnlabel
-    }:SetupBoxProps) => {
+const SetupBox = ({
+                      dashed = false,
+                      header = "",
+                      text = "",
+                      btnicon = "",
+                      btnlabel = "",
+                      button
+                  }: SetupBoxProps) => {
     return (
-        <div className={`setup-box ${dashed?" dashed":""}`}>
-           <div className="info">
-             <h3>{header}</h3>
-             <p className="item">{text}</p>
-           </div>
-           <Button icon={btnicon} label={btnlabel} type="default" />
-         </div>
+        <div className={`setup-box ${dashed ? " dashed" : ""}`}>
+            <div className="info">
+                <h3>{header}</h3>
+                <p className="item">{text}</p>
+            </div>
+            { btnlabel !== "" && <Button icon={btnicon} label={btnlabel} type="default"/> }
+            { btnlabel === "" && button}
+        </div>
     )
 }
 
