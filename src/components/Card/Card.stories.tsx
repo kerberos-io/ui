52610edd75de0c0b56438d9c498ba30f3a33518f
Thomas Quandalle <cedric@kerberos.io>
2022-06-14 14:38:14 +0200

    update card layout

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -13,5 +13,9 @@ export default {
 const Template: Story<CardProps> = (args) => <Card {...args} />;
 
 // Reuse that template for creating different stories
-export const Default = Template.bind({});
-Default.args = { label: "Default Button", icon: "dashboard", type: "" };
+export const CameraConnected = Template.bind({});
+CameraConnected.args = { title: "Camera", subtitle: "Succesfully connected", footer: "IP Camera", icon: "circle-check-big", type: "" };
+
+
+export const HubDisconnected = Template.bind({});
+HubDisconnected.args = { title: "Cloud", subtitle: "Not connected", footer: "Kerberos Hub", icon: "circle-cross-big", type: "" };
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,6 +5,9 @@ import "./card.scss";
 export interface CardProps  {
     label: string;
     icon: string;
+    title: string;
+    subtitle: string;
+    footer: string;
     type?: string;
     onClick?: (
         event: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -14,11 +17,27 @@ export interface CardProps  {
 const Card = ({
                 onClick,
                 label,
+                title = "",
+                subtitle = "",
+                footer = "",
                 type = "neutral",
                 icon,
             }: CardProps) => {
     return (
-        <div>My Card</div>
+        <div className="card">
+            <div className="title">
+                <div className="description">
+                    <h1>{title}</h1>
+                    <p>{subtitle}</p>
+                </div>
+                <div className="image">
+                    <Icon label={icon}></Icon>
+                </div>
+            </div>
+            <div className="footer">
+                <div className="text">{footer}</div>
+            </div>
+        </div>
     );
 };
 
